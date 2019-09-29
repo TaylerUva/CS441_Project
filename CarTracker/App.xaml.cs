ab1e18e48af2059cbcfd4fc1b1bf053c114f8173
Marilyn S <sanch341@cougars.csusm.edu>
2019-09-29 11:49:13 -0700

    Revert "Add button to main page to goes to car service page :-)"
    This reverts commit b2bd1f26fb1008110564391ca77d0a1f6cc56cb1.

--- CarTracker/App.xaml.cs
+++ CarTracker/App.xaml.cs
@@ -2,30 +2,23 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace CarTracker
-{
-    public partial class App : Application
-    {
-        public App()
-        {
+namespace CarTracker {
+    public partial class App : Application {
+        public App() {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new MainPage());
-
+            MainPage = new MainPage();
         }
 
-        protected override void OnStart()
-        {
+        protected override void OnStart() {
             // Handle when your app starts
         }
 
-        protected override void OnSleep()
-        {
+        protected override void OnSleep() {
             // Handle when your app sleeps
         }
 
-        protected override void OnResume()
-        {
+        protected override void OnResume() {
             // Handle when your app resumes
         }
     }
--- CarTracker/MainPage.xaml.cs
+++ CarTracker/MainPage.xaml.cs
@@ -6,23 +6,13 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace CarTracker
-{
+namespace CarTracker {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-
-    public partial class MainPage : ContentPage
-    {
-        public MainPage()
-        {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
             InitializeComponent();
         }
-        async void Next_Page_Clicked(object sender, System.EventArgs e)
-        {
-            await Navigation.PushAsync(new CarServicePage());
-        }
-
     }
-
 }

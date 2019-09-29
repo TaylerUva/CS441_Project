d575251d149aa4accfcfef16747cd190c54c570e
Marilyn S <sanch341@cougars.csusm.edu>
2019-09-29 12:01:00 -0700

    New Button at Bottom of Main Page
    There is now a button on the main page that goes to Car Service Page.

--- CarTracker/App.xaml.cs
+++ CarTracker/App.xaml.cs
@@ -2,23 +2,30 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace CarTracker {
-    public partial class App : Application {
-        public App() {
+namespace CarTracker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
+
         }
 
-        protected override void OnStart() {
+        protected override void OnStart()
+        {
             // Handle when your app starts
         }
 
-        protected override void OnSleep() {
+        protected override void OnSleep()
+        {
             // Handle when your app sleeps
         }
 
-        protected override void OnResume() {
+        protected override void OnResume()
+        {
             // Handle when your app resumes
         }
     }

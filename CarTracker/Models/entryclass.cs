b0caf59fca81b7bfcc2b72e7cf2d5c7e3bbf3b3d
yanxilinn <44556484+yanxilinn@users.noreply.github.com>
2019-10-02 15:48:43 -0700

    Update entryclass.cs

--- CarTracker/Models/entryclass.cs
+++ CarTracker/Models/entryclass.cs
@@ -2,15 +2,22 @@ using system;
 namespace CarTracker.Models
 {
 
+public enum Car {
+        //TODO: Add more car service
+        MPG,
+        Gas,
+        ###,
+    };
+
   public class Entry
     {
       public string date { get; }
       public int millage { get; }
       public string location { get; }
       public string description { get; }
-      public car carService { get; }
+      public Car carService { get; }
         
-    public Entry (string entryDate, int entryMillage, string entryLocation, string entryDescriprion, car entryCarService)
+    public Entry (string entryDate, int entryMillage, string entryLocation, string entryDescriprion, Car entryCarService)
 
       date = entryDate;
       millage = entryMillage;

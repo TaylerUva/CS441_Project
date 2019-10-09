daadb055e76f766023371c73ae18a085e6849977
AngelaKimmy <angelakim.la@gmail.com>
2019-10-09 16:57:15 -0700

    Implemented cars sort by license plate

--- CarTracker/YourCarsPage.xaml.cs
+++ CarTracker/YourCarsPage.xaml.cs
@@ -6,6 +6,7 @@ using Xamarin.Forms;
 
 namespace CarTracker {
     public partial class YourCarsPage : ContentPage {
+
         public static ObservableCollection<Car> Cars = new ObservableCollection<Car>();
         public YourCarsPage() {
             InitializeComponent();
@@ -39,7 +40,31 @@ namespace CarTracker {
         }
 
         private void SortByPlate() {
-            //Cars.Sort();
+            //CarSort sorting = new CarSort();
+            //List <Car> tempList = new List<Car>(Cars);
+            //tempList.Sort((IComparer<Car>)sorting);
+            //Cars = new ObservableCollection<Car>(Cars.OrderBy(i => i));
+
+
+            List<Car> tempList = new List<Car>(Cars);
+            int minIndex = 0;
+            
+            for(int i = 0; i < tempList.Count; i ++)
+            {
+                minIndex = i;
+                for(int unsort = i + 1; unsort < tempList.Count; unsort++)
+                {
+                    if (string.Compare(tempList[unsort].plate, tempList[minIndex].plate) == -1)
+                    {
+                        minIndex = unsort;
+                    }
+                }
+                Car tempCar = tempList[minIndex];
+                tempList[minIndex] = tempList[i];
+                tempList[i] = tempCar;
+            }
+            Cars = new ObservableCollection<Car>(tempList);
+            yourCarsList.ItemsSource = Cars;
         }
         private void SortByMake() {
 

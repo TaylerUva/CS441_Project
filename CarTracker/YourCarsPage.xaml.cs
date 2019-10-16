aaa9e5914fd6444ecd5ab999462ffcbea32899ae
Tayler Uva <tayleruva@gmail.com>
2019-10-16 16:41:16 -0700

    Revert "Revert "Update YourCarsPage.xaml.cs""
    This reverts commit f05e6e346ff0a4c2d0ebe357d59d13c8af730bff.

--- CarTracker/YourCarsPage.xaml.cs
+++ CarTracker/YourCarsPage.xaml.cs
@@ -48,8 +48,55 @@ namespace CarTracker {
 
         }
         private void SortByColor() {
+             public Color()
+            {
+                this.Title = "Car Color";
+                List<Car> cars = new List<Car>();
+                cars.Add(new color() { color = "Beige" });
+                cars.Add(new color() { color = "Black" });
+                cars.Add(new color() { color = "Blue" });
+                cars.Add(new color() { color = "Bronze" });
+                cars.Add(new color() { color = "Brown" });
+                cars.Add(new color() { color = "Burgundy" });
+                cars.Add(new color() { color = "Cream" });
+                cars.Add(new color() { color = "Red(Dark)" });
+                cars.Add(new color() { color = "Blue(Dark)" });
+                cars.Add(new color() { color = "Brown(Dark)" });
+                cars.Add(new color() { color = "Green(Dark)" });
+                cars.Add(new color() { color = "Grey(Dark)" });
+                cars.Add(new color() { color = "Gold" });
+                cars.Add(new color() { color = "Green" });
+                cars.Add(new color() { color = "Grey" });
+                cars.Add(new color() { color = "Blue(Light)" });
+                cars.Add(new color() { color = "Brown(Light)" });
+                cars.Add(new color() { color = "Green(Light)" });
+                cars.Add(new color() { color = "Grey(Light)" });
+                cars.Add(new color() { color = "Maroon" });
+                cars.Add(new color() { color = "Mauve" });
+                cars.Add(new color() { color = "Multiple Colors" });
+                cars.Add(new color() { color = "Orange" });
+                cars.Add(new color() { color = "Pink" });
+                cars.Add(new color() { color = "Purple" });
+                cars.Add(new color() { color = "Red" });
+                cars.Add(new color() { color = "Rust" });
+                cars.Add(new color() { color = "Silver" });
+                cars.Add(new color() { color = "Tan" });
+                cars.Add(new color() { color = "Teal" });
+                cars.Add(new color() { color = "Unknown" });
+                cars.Add(new color() { color = "White" });
+                cars.Add(new color() { color = "Wine" });
+                cars.Add(new color() { color = "Yellow" });
 
+                StackLayout stackLayout = new StackLayout();
+                SortByColor = new Picker();
+                SortByColor.Title = "Choose Car Color";
+                SortByColor.ItemsSource = colors;
+                stackLayout.Children.Add(SortByColor);
+
+                Content = stackLayout;
+            }
         }
+        
         private void SortByVin() {
 
         }

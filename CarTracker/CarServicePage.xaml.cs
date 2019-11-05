cac29a71197ca6c8ba2fe493ee5fc6ec111c7f64
park-moonkyu <mk_crew@naver.com>
2019-11-04 17:15:19 -0800

    Change
    Mileage Change

--- CarTracker/CarServicePage.xaml.cs
+++ CarTracker/CarServicePage.xaml.cs
@@ -48,7 +48,47 @@ namespace CarTracker
 
         private void OnSortClicked(object sender, System.EventArgs e)
         {
-          
+
+        }
+
+        private void TextChange(object sender, TextChangedEventArgs e)
+        {
+            var entry = (Entry)sender;
+            try
+            {
+
+                if (entry.Text.Length > 7)
+                {
+                    string entryText = entry.Text;
+
+                    entry.TextChanged -= TextChange;
+
+                    entry.Text = e.OldTextValue;
+                    entry.TextChanged += TextChange;
+                }
+                else if (!entry.Text.All(char.IsDigit))
+                {
+                    string entryText = entry.Text;
+
+                    entry.TextChanged -= TextChange;
+
+                    entry.Text = e.OldTextValue;
+                    entry.TextChanged += TextChange;
+                    return;
+                }
+                string strName = entry.Text;
+
+                if (strName.Contains(".") || strName.Contains("-"))
+                {
+                    strName = strName.Replace(".", "").Replace("-", "");
+                    entry.Text = strName;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception caught: {0}", ex);
+            }
         }
 
     }

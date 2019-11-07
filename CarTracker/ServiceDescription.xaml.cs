b1c20278a8ceda65c6ee9a24008ae707068f544f
Marilyn S <sanch341@cougars.csusm.edu>
2019-11-06 17:33:22 -0800

    new service description pages created

---
+++ CarTracker/ServiceDescription.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CarTracker
+{
+    public partial class ServiceDescription : ContentPage
+    {
+        public ServiceDescription()
+        {
+            InitializeComponent();
+        }
+    }
+}

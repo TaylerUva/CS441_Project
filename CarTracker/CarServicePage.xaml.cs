ac0b6791172221d66807bafa07fff3e37e8c59fc
Marilyn S <sanch341@cougars.csusm.edu>
2019-11-29 16:58:19 -0800

    Added new logo, background images, and made text centered on service page

--- CarTracker/CarServicePage.xaml.cs
+++ CarTracker/CarServicePage.xaml.cs
@@ -34,14 +34,14 @@ namespace CarTracker {
         }
 
         private void ConfirmNewName(object sender, System.EventArgs e) {
-            Service newService = new Service(date.Date.ToString(), millage.Text, location.Text, description.Text, car.Text);
+            Service newService = new Service(date.Text, millage.Text, location.Text, description.Text, car.Text);
             Services.Add(newService);
             ServiceView.IsVisible = false;
             ClearEntryFields();
         }
 
         private void ClearEntryFields() {
-            date.Date = DateTime.Now;
+            date.Text = null;
             millage.Text = null;
             location.Text = null;
             description.Text = null;

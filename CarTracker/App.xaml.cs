a08fd1805a6d1f7e36a49b316822c1e51bae0d9e
Tayler Uva <tayleruva@gmail.com>
2019-11-29 21:09:06 -0800

    Changed nav bar

--- CarTracker/App.xaml.cs
+++ CarTracker/App.xaml.cs
@@ -9,40 +9,39 @@ namespace CarTracker {
     public partial class App : Application {
         public static string FilePath;
 
-        public App()
-        {
+        public App() {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new MainPage());
+            MainPage = new NavigationPage(new MainPage()) { };
 
         }
 
-        public App(string filePath)
-        {
+        public App(string filePath) {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new MainPage());
+            MainPage = new NavigationPage(new MainPage()) {
+                BarBackgroundColor = Color.DarkSlateGray,
+                BarTextColor = Color.White,
+                Title = "CarTracker"
+
+            };
 
             FilePath = filePath;
         }
 
-        protected override void OnStart()
-        {
+        protected override void OnStart() {
             // Handle when your app starts
-            using (SQLiteConnection conn = new SQLiteConnection(App.FilePath))
-            {
+            using (SQLiteConnection conn = new SQLiteConnection(App.FilePath)) {
                 conn.CreateTable<Car>();
                 conn.CreateTable<Service>();
             }
         }
 
-        protected override void OnSleep()
-        {
+        protected override void OnSleep() {
             // Handle when your app sleeps
         }
 
-        protected override void OnResume()
-        {
+        protected override void OnResume() {
             // Handle when your app resumes
         }
     }
--- CarTracker/MainPage.xaml.cs
+++ CarTracker/MainPage.xaml.cs
@@ -6,27 +6,23 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace CarTracker
-{
+namespace CarTracker {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
 
-    public partial class MainPage : ContentPage
-    {
-        public MainPage()
-        {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
             InitializeComponent();
-           myCarPic.Source = "carService";
+            NavigationPage.SetHasNavigationBar(this, false);
+            myCarPic.Source = "carService";
         }
-		
-		async void YourCarsClicked(object sender, System.EventArgs e)
-		{
-			await Navigation.PushAsync(new YourCarsPage());
-		}
 
-		async void Next_Page_Clicked(object sender, System.EventArgs e)
-        {
+        async void YourCarsClicked(object sender, System.EventArgs e) {
+            await Navigation.PushAsync(new YourCarsPage());
+        }
+
+        async void Next_Page_Clicked(object sender, System.EventArgs e) {
             await Navigation.PushAsync(new CarServicePage());
         }
 
